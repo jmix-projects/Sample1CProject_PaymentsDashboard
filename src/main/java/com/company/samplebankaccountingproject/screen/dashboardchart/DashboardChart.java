5f8760f2f2d343ce89506e8f5fb99e75e0b3643e
Alex <alex@kavlab.ru>
2022-10-06 16:25:35 +0400

    dashboard chart

---
+++ src/main/java/com/company/samplebankaccountingproject/screen/dashboardchart/DashboardChart.java
@@ -0,0 +1,93 @@
+package com.company.samplebankaccountingproject.screen.dashboardchart;
+
+import com.company.samplebankaccountingproject.app.ExchangeOData;
+import com.company.samplebankaccountingproject.entity.Payment;
+import io.jmix.charts.component.PieChart;
+import io.jmix.core.DataManager;
+import io.jmix.core.entity.KeyValueEntity;
+import io.jmix.dashboardsui.annotation.DashboardWidget;
+import io.jmix.ui.Notifications;
+import io.jmix.ui.component.Button;
+import io.jmix.ui.data.impl.MapDataItem;
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.Subscribe;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+@UiController("DashboardChart")
+@UiDescriptor("dashboard-chart.xml")
+@DashboardWidget(name = "Payments-chart")
+public class DashboardChart extends ScreenFragment {
+    @Autowired
+    private PieChart pie3dChart;
+    @Autowired
+    private DataManager dataManager;
+    @Autowired
+    private Button loadFrom1C;
+    @Autowired
+    private ExchangeOData exchangeOData;
+    @Autowired
+    private Notifications notifications;
+
+    private void updateChart() {
+        List<KeyValueEntity> values =
+                dataManager.loadValues("select customer.name, sum(e.sum) total " +
+                                "from Payment e " +
+                                "left join e.customer customer " +
+                                "group by customer.name")
+                        .properties("name", "sum")
+                        .list();
+        for (KeyValueEntity value : values) {
+            pie3dChart.addData(
+                    MapDataItem.of("customer", value.getValue("name"),
+                            "sum", value.getValue("sum"))
+            );
+        }
+    }
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        if(dataManager.load(Payment.class)
+                .query("select p from Payment p")
+                .optional()
+                .isEmpty()) {
+            pie3dChart.setVisible(false);
+            loadFrom1C.setVisible(true);
+        } else {
+            pie3dChart.setVisible(true);
+            loadFrom1C.setVisible(false);
+            updateChart();
+        }
+    }
+
+    @Subscribe("loadFrom1C")
+    public void onLoadFrom1CClick(Button.ClickEvent event) {
+        try {
+
+            exchangeOData.loadCustomers();
+            exchangeOData.loadBankAccounts();
+            exchangeOData.loadIncomingDescriptions();
+            exchangeOData.loadDocuments();
+
+            notifications.create()
+                    .withDescription("Data has been loaded")
+                    .withPosition(Notifications.Position.BOTTOM_LEFT)
+                    .withHideDelayMs(500)
+                    .show();
+
+            loadFrom1C.setVisible(false);
+            pie3dChart.setVisible(true);
+            updateChart();
+
+        } catch (Exception e) {
+            notifications.create(Notifications.NotificationType.ERROR)
+                    .withCaption("Error")
+                    .withDescription(e.getMessage())
+                    .show();
+        }
+    }
+}
