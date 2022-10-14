a72be38d23041532d045808f4319e2dff16008c5
Alex <alex@kavlab.ru>
2022-10-14 11:50:56 +0400

    added screens

--- src/main/java/com/company/samplebankaccountingproject/screen/payment/PaymentBrowse.java
+++ src/main/java/com/company/samplebankaccountingproject/screen/payment/PaymentBrowse.java
@@ -1,10 +1,38 @@
 package com.company.samplebankaccountingproject.screen.payment;
 
+import com.company.samplebankaccountingproject.app.ExchangeOData;
+import io.jmix.core.DataManager;
+import io.jmix.ui.Notifications;
+import io.jmix.ui.component.Button;
+import io.jmix.ui.model.CollectionLoader;
 import io.jmix.ui.screen.*;
 import com.company.samplebankaccountingproject.entity.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Payment.browse")
 @UiDescriptor("payment-browse.xml")
 @LookupComponent("paymentsTable")
 public class PaymentBrowse extends StandardLookup<Payment> {
+    @Autowired
+    private CollectionLoader<Payment> paymentsDl;
+    @Autowired
+    private ExchangeOData exchangeOData;
+    @Autowired
+    private Notifications notifications;
+
+    @Subscribe("paymentsLoadBtn")
+    public void onPaymentsLoadBtnClick(Button.ClickEvent event) {
+        try {
+            exchangeOData.loadCustomers();
+            exchangeOData.loadIncomingDescriptions();
+            exchangeOData.loadBankAccounts();
+            exchangeOData.loadDocuments();
+            paymentsDl.load();
+        } catch (Exception e) {
+            notifications.create(Notifications.NotificationType.ERROR)
+                    .withCaption("Load payments error")
+                    .withDescription(e.toString())
+                    .show();
+        }
+    }
 }
